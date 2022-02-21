c451a047a3da7b6a8a63df678c573e124c2d7071
Kayani Pereira <kayani.andrade23@gmail.com>
2022-02-21 19:33:55 -0300

    initial commit

---
+++ IPFind/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import ipapi
+
+
+# Create your views here.
+
+
+def Index(request):
+
+        search = request.POST.get('search')
+     
+        data = ipapi.location(ip=search, output='json')
+
+        context = {"data": data}
+
+        return render(request, 'index.html', context)
